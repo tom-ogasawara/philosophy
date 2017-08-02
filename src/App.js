5c3c70accff929cda8ba51cf299ce2d93e5ea2f9
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-08-02 16:51:42 -0700

    Update search logic

--- src/App.js
+++ src/App.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ''
+    };
+  }
+
+  search() {
+    const BASE_URL = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json'
+    let FETCH_URL = `${BASE_URL}${this.state.query}`;
+
+    fetch(URL, {
+      method: 'GET'
+    })
+      // .then(response => response.json())
+      .then(json1 => {
+        console.log('json1: ', json1);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Path To Philosophy</h2>
+        <div className="App-header">Getting to Philosophy</div>
+        <div className="search-bar">
+          <div className="App-body">
+            <input
+              type="text"
+              placeholder="Search Wikipedia"
+              value={this.state.query}
+              onChange={event => {
+                this.setState({ query: event.target.value });
+              }}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <div onClick={() => this.search()}>
+              search
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          Search: 
-          <br />
-          <textarea />
-        </p>
       </div>
     );
   }
---
+++ src/search.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import './App.css';
+import './reset.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sequence: null,
+      sequenceLength: 0,
+      features: []
+    };
+  }
+
+  search() {
+    const URL = 'https://teselagen.com/getSequence';
+
+    fetch(URL, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(json => {
+        const sequence = json.sequence;
+        const sequenceLength = json.sequenceLength;
+        const features = json.features;
+
+        this.setState({
+          sequence,
+          sequenceLength,
+          features
+        });
+      });
+  }
+
+  renderLabel() {
+    return (
+      <div className="plasmid-label">
+        <div className="zero-bp-label">0 bp</div>
+        <div className="zero-bp-label">|</div>
+        <div className="plasmid-name">Plasmid X</div>
+        <div className="sequence-length">
+          {this.state.sequenceLength}00 bp
+        </div>
+      </div>
+    );
+  }
+
+  renderFeatures() {
+    const featureList = this.state.features;
+    const featureDivs = [];
+
+    for (let i = 0; i < this.state.features.length; i++) {
+      let degrees = featureList[i].index / this.state.sequenceLength * 360;
+      let rotate = { transform: `rotate(${degrees}deg)` };
+      let align = { transform: `rotate(-${degrees}deg)` };
+
+      featureDivs.push(
+        <div className="feature" key={i} style={rotate}>
+          <div className="feature-label" style={align}>
+            {featureList[i].name} <br />
+            {featureList[i].index}00 bp <br />
+          </div>
+          <br />
+          <br />|
+        </div>
+      );
+    }
+
+    return featureDivs;
+  }
+
+  renderSequence() {
+    return (
+      <div className="sequence">
+        Sequence: <br />
+        {this.state.sequence}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="outer-container">
+          <div className="container">
+            <div className="app-title">Plasmid Visualization</div>
+            <div className="inner-container">
+              <Button
+                className="search-button"
+                onClick={() => this.search()}
+                bsStyle="primary"
+                bsSize="large"
+              >
+                Load New Plasmid
+              </Button>
+              {this.state.sequence !== null
+                ? <div className="visualization">
+                    <div className="data">
+                      <div className="circle" />
+                      {this.renderLabel()}
+                      {this.renderFeatures()}
+                    </div>
+                  </div>
+                : <div />}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
