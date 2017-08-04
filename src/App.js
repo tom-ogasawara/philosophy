6d9a08831ac97144224c8b76dccf403fc4a6c80d
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-08-03 17:24:22 -0700

    Prepare app for temporary deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -11,41 +12,52 @@ class App extends Component {
   }
 
   search() {
-    const BASE_URL = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json'
+    const BASE_URL =
+      'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json';
     let FETCH_URL = `${BASE_URL}${this.state.query}`;
 
-    fetch(URL, {
-      method: 'GET'
-    })
-      // .then(response => response.json())
-      .then(json1 => {
-        console.log('json1: ', json1);
+    //   fetch(FETCH_URL, {
+    //     mode: 'cors',
+    //     method: 'GET'
+    //   })
+    //     // .then(response => response.json())
+    //     .then(json1 => {
+    //       console.log('json1: ', json1);
+    //     });
+    // }
+
+    axios
+      .get(FETCH_URL)
+      .then(function(response) {
+        console.log('response: ', response);
+      })
+      .catch(function(error) {
+        console.log('error: ', error);
       });
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">Getting to Philosophy</div>
-        <div className="search-bar">
-          <div className="App-body">
-            <input
-              type="text"
-              placeholder="Search Wikipedia"
-              value={this.state.query}
-              onChange={event => {
-                this.setState({ query: event.target.value });
-              }}
-              onKeyPress={event => {
-                if (event.key === 'Enter') {
-                  this.search();
-                }
-              }}
-            />
-            <div onClick={() => this.search()}>
-              search
-            </div>
-          </div>
+      <div className="app">
+        <div className="app-header">Getting to Philosophy</div>
+        <div className="app-body">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search Wikipedia"
+            value={this.state.query}
+            onChange={event => {
+              this.setState({ query: event.target.value });
+            }}
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+                this.search();
+              }
+            }}
+          />
+          <button className="search-button" onClick={() => this.search()}>
+            search
+          </button>
         </div>
       </div>
     );
