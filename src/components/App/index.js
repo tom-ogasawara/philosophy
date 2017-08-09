115160b7ed8f19ac83d3503fe412eb6644e4cf4b
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-08-08 22:10:40 -0700

    Add loop checking to parser

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,33 +12,42 @@ class App extends Component {
       textBody: '',
       stepsTaken: 0,
       pathFound: false,
-      giveUp: false
+      giveUp: false,
+      looping: false,
+      visitedLinks: []
     };
   }
 
   searchWiki() {
-    const { query, stepsTaken, giveUp }= this.state;
+    const { query, stepsTaken, visitedLinks } = this.state;
+    const linkArray = visitedLinks;
+    // console.log('visitedLinks: ', visitedLinks);
     const URL = `https://en.wikipedia.org/w/api.php?action=parse&page=${query}&prop=text&origin=*&format=json`;
-   
+
     // Check whether we found a path to philosophy
     if (query === 'Philosophy') {
       this.setState({ pathFound: true });
-      console.log('pathFound: ', this.state.pathFound);
       return;
 
-    //  Give up if we don't find it in 100 steps
-    } else if (stepsTaken === 100) {
+      // Check if we're stuck in an infinite loop
+    } else if (visitedLinks.includes(query)) {
+      this.setState({ looping: true, giveUp: true });
+      return;
+
+      //  Give up if we don't find it in 100 steps
+    } else if (stepsTaken === 20) {
       this.setState({ giveUp: true });
       return;
 
-    //  If we didn't find a path, start a new search
+      //  If we didn't find a path, start a new search
     } else {
       axios
         .get(URL)
         .then(response => {
-          console.log('response: ', response);
+          // console.log('response: ', response);
           const textBody = Object.values(response.data.parse.text)[0];
-          this.setState({ textBody });
+          linkArray.push(query);
+          this.setState({ textBody, visitedLinks: linkArray });
           this.parseWiki();
         })
         .catch(error => {
@@ -48,32 +57,20 @@ class App extends Component {
   }
 
   parseWiki() {
-    const { pathFound, textBody, stepsTaken } = this.state;
+    const { pathFound, textBody, stepsTaken, visitedLinks } = this.state;
+    
     let link = '';
 
     // Make sure you're in the main body of the article
     let bodyStart = textBody.indexOf('<p>');
-    // let bodyStop = textBody.indexOf('</p>');
     let mainBody = textBody.slice(bodyStart);
-
-    // if (!mainBody.includes('<a href=')) {
-    //   mainBody = textBody.slice(bodyStop + 1);
-    //   bodyStart = mainBody.indexOf('<p>');
-    //   bodyStop = mainBody.indexOf('</p>');
-    //   mainBody = mainBody.slice(bodyStart, bodyStop);
-    //   console.log("WOAH");
-    // }
 
     // Find the first link in the main body
     const linkStart = mainBody.indexOf('<a href="/wiki/');
     const linkStop = mainBody.indexOf('</a>');
     const firstParens = mainBody.indexOf(')');
-    // if (firstParens < linkStart) {
-
-    // }
-
     const fullLink = mainBody.slice(linkStart + 15, linkStop + 4);
-    console.log('fullLink: ', fullLink);
+
     // Find the title of the link
     const titleStart = fullLink.indexOf('title=');
     for (let i = 0; i < fullLink.length; i++) {
@@ -87,13 +84,19 @@ class App extends Component {
     link.replace(' ', '_');
 
     // Record our progress
-    this.setState({ query: link, stepsTaken: stepsTaken + 1 });
-    console.log('link: ', link);
+    
+    this.setState({
+      query: link,
+      stepsTaken: stepsTaken + 1
+    });
+    // console.log('link: ', link);
     this.searchWiki();
   }
 
   render() {
     const { pathFound, stepsTaken, giveUp } = this.state;
+    const infoStatus = pathFound || giveUp ? 'info-container' : 'hidden';
+    console.log('this.state.visitedLinks: ', this.state.visitedLinks);
     return (
       <div className="app">
         <div className="app-body">
@@ -114,13 +117,13 @@ class App extends Component {
           <button className="search-button" onClick={() => this.searchWiki()}>
             Search
           </button>
-          <div className="info-container">
-            <ResultDisplay 
-              pathFound={pathFound}
-              stepsTaken={stepsTaken}
-              giveUp={giveUp}
-            />
-          </div>
+        </div>
+        <div className={infoStatus}>
+          <ResultDisplay
+            pathFound={pathFound}
+            stepsTaken={stepsTaken}
+            giveUp={giveUp}
+          />
         </div>
       </div>
     );
